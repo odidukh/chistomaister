22950f42d11cfd53442d6d5d03d8aecd54f99abc
Oles Didukh <oles.didukh@gmail.com>
2023-01-12 00:37:28 +0200

    Add Navbar Dropdowns

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import HeaderDropdown from './HeaderDropdown';
 
 const HEADER_MENU_ITEMS = [
-    { id: 'services', name: 'Послуги', link: '/services' },
+    {
+        id: 'services',
+        name: 'Послуги',
+        link: '/services',
+        options: [
+            { id: 'cleaning', name: 'Миття', link: '/services/cleaning' },
+            { id: 'ironing', name: 'Глаження', link: '/services/ironing' },
+            { id: 'laundry', name: 'Пральня', link: '/services/laundry' },
+            {
+                id: 'dry-cleaning',
+                name: 'Хімчистка',
+                link: '/services/dry-cleaning',
+            },
+            {
+                id: 'carpet-cleaning',
+                name: 'Миття килимів',
+                link: '/services/carpet-cleaning',
+            },
+            {
+                id: 'window-cleaning',
+                name: 'Миття вікон',
+                link: '/services/window-cleaning',
+            },
+            {
+                id: 'household-chores',
+                name: 'Рутинні роботи',
+                link: '/services/household-chores',
+            },
+            { id: 'other', name: 'Інші послуги', link: '/services/other' },
+        ],
+    },
     { id: 'about', name: 'Про нас', link: '/about' },
     { id: 'photos', name: 'Фото', link: '/photos' },
     { id: 'prices', name: 'Ціни', link: '/prices' },
@@ -14,9 +45,15 @@ function HeaderMenu() {
     return (
         <div className="header-menu__items">
             {HEADER_MENU_ITEMS.map((item) => (
-                <a key={item.id} href={item.link} className="header-menu__item">
-                    {item.name}
-                </a>
+                <HeaderDropdown
+                    key={item.id}
+                    name={item.name}
+                    link={item.link}
+                    options={item.options}
+                />
+                // <a key={item.id} href={item.link} className="header-menu__item">
+                //     {item.name}
+                // </a>
             ))}
         </div>
     );
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Services() {
     return (
-        <div className='services-page'>
-            <h1>Проводите багато часу вдома?</h1>
-            
+        <div className="services-page">
+            <div className='left-side'>
+                <h1>Проводите багато часу вдома?</h1>
+                <h1>Прагните чистоти і комфорту?</h1>
+            </div>
+
+            <div className='right-side'>
+                <h1>Ми допоможемо вам!</h1>
+                <h1>Замовте послуги нашого<br/><span>Майстра Чистоти!</span></h1>
+            </div>
         </div>
     );
 }
